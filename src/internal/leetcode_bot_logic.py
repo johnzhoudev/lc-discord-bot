31ffa985798f0acf69a9f04bcba3d7dca10d0e2f
johnzhoudev <john.zhou@stackadapt.com>
2025-06-21 16:59:41 -0400

    Added scheduling loop logic

--- src/internal/leetcode_bot_logic.py
+++ src/internal/leetcode_bot_logic.py
@@ -13,7 +13,7 @@ from src.types.errors import (
 from src.internal.leetcode_client import LeetcodeClient
 from src.internal.posts import Post, PostGenerator, Scheduler
 from enum import Enum
-from typing import Dict
+from typing import Dict, Optional
 
 from src.utils.string import parse_date_str
 from src.utils.text import get_question_text, get_schedule_post_response_text
@@ -29,7 +29,7 @@ log = logging.getLogger("LeetcodeBot (Internal Logic)")
 
 class LeetcodeBot:
     channels: Dict[Channel, TextChannel] = {}
-    scheduled_posts: list[Scheduler] = []
+    schedulers: list[Scheduler] = []
     uncompleted_questions: set[str] = set()
     completed_questions: set[str] = set()
     log: logging.Logger = logging.getLogger("Leetcode Bot")
@@ -69,9 +69,9 @@ class LeetcodeBot:
                 return
 
             def should_post(curtime):
-                return curtime < date
+                return curtime > date
 
-            self.scheduled_posts.append(
+            self.schedulers.append(
                 Scheduler(
                     PostGenerator(args.url, desc=args.desc, story=args.story),
                     should_post,
@@ -85,14 +85,22 @@ class LeetcodeBot:
             post = PostGenerator(**args.model_dump(exclude_none=True))()
             await self.post_question(post)
 
-    async def handle_check_for_scheduled_posts(self):
+    async def handle_view_schedulers(self):
+        text = (
+            "\n".join([str(scheduler) for scheduler in self.schedulers])
+            or "No schedulers active."
+        )
+        await self.send(text, Channel.BOT)
+
+    async def handle_check_for_schedulers(self):
         curtime = datetime.now()
 
         # Make shallow copy so can be reused
-        for scheduled_post in self.scheduled_posts[:]:  # noqa
+        for scheduled_post in self.schedulers[:]:  # noqa
             if not scheduled_post.should_post(curtime):
                 continue
 
+            log.info(f"Scheduling post {scheduled_post.id}")
             post = scheduled_post.get_post()
 
             if not post:
@@ -102,14 +110,19 @@ class LeetcodeBot:
             try:
                 await self.post_question(post)
             except FailedScrapeError as e:
-                await self.handle_error(e)
-                await self.send(f"Removing question {scheduled_post}", Channel.BOT)
-                self.scheduled_posts.remove(scheduled_post)
+                await self.handle_error(e, f"Removing question {scheduled_post}")
+                self.schedulers.remove(scheduled_post)
                 continue
 
             if scheduled_post.should_delete():
-                self.scheduled_posts.remove(scheduled_post)
+                self.schedulers.remove(scheduled_post)
 
-    async def handle_error(self, error: Error):
+    async def handle_error(
+        self, error: Error, additional_messages: Optional[str] = None
+    ):
         log.error(error.msg)
-        await self.send(error.displayed_msg, Channel.BOT)
+        log.info(additional_messages)
+        displayed_msg = error.displayed_msg + (
+            f"\n{additional_messages}" if additional_messages else ""
+        )
+        await self.send(displayed_msg, Channel.BOT)
